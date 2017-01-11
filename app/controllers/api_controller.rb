e7307ccbc2919e06169e75bc7564e35e6765c15e
Izotov Denis <izotov87@gmail.com>
2017-01-11 22:27:38 +1000

    fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,7 +9,7 @@ class ApiController < ApplicationController
       @can_view_price = current_user.price_type
       ["buyer","dev","admin", "retail"].include?(current_user.role) ? @can_view_qty = true : @can_view_price = false
     else
-      @can_view_price = 'retail'
+      @can_view_price = '0fa9bc8a-166f-11e0-9aa1-001e68eacf93'
       @can_view_qty = false
     end
   end
